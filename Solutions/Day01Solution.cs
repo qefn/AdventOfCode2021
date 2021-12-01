bec71221175a5dfcbdec30d1d894bf569e0e062e
Sebastian Weis <sebastian.weis@gmx.at>
2021-12-01 10:27:05 +0100

    Solution for Day 01 Stage 2

--- Solutions/Day01Solution.cs
+++ Solutions/Day01Solution.cs
@@ -2,11 +2,14 @@ namespace AdventOfCode2021.Solutions {
     public class Day01Solution : Solution {
         public override int Id => 1;
 
-        public override List<Action> Stages => new List<Action> { Stage1 };
+        public override List<Action> Stages => new List<Action> { () => Stage1(), Stage2 };
 
-        private void Stage1() {
-            List<string> lines = ReadInputFile("Day01Stage01.txt");
-            List<int> measurements = lines.Select(line => int.Parse(line)).ToList();
+        private void Stage1(List<int>? measurements = null) {
+            if (measurements == null) {
+                List<string> lines = ReadInputFile("Day01Stage01.txt");
+                measurements = lines.Select(line => int.Parse(line)).ToList();
+            }
+
             int increases = 0;
             for (int i = 1; i < measurements.Count; i++) {
                 int previousMeasurement = measurements[i - 1];
@@ -14,6 +17,16 @@ namespace AdventOfCode2021.Solutions {
                 increases += previousMeasurement < currentMeasurement ? 1 : 0;
             }
             Console.WriteLine($"The depth increased {increases} times.");
+        }
+
+        private void Stage2() {
+            List<string> lines = ReadInputFile("Day01Stage02.txt");
+            List<int> measurements = lines.Select(line => int.Parse(line)).ToList();
+            List<int> measurementSums = new List<int>();
+            for (int i = 0; i < measurements.Count - 2; i++) {
+                measurementSums.Add(measurements[i] + measurements[i + 1] + measurements[i + 2]);
+            }
+            Stage1(measurementSums);
         }
     }
 }
