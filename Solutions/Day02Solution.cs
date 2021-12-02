d44d0cb12328279686a6d83d30056257021c49f8
Sebastian Weis <sebastian.weis@gmx.at>
2021-12-02 17:15:51 +0100

    Day 2 solution

---
+++ Solutions/Day02Solution.cs
@@ -0,0 +1,62 @@
+namespace AdventOfCode2021.Solutions {
+    public class Day02Solution : Solution {
+        public override int Id => 2;
+
+        public override List<Action> Stages => new List<Action> { Stage1, Stage2 };
+
+        private List<Tuple<Direction, int>> GetCourse(string inputFileName) {
+            List<string> lines = ReadInputFile(inputFileName);
+            return lines.Select(line => {
+                string[] parameters = line.Split(" ");
+                return new Tuple<Direction, int>(Enum.Parse<Direction>(parameters[0]), int.Parse(parameters[1]));
+            }).ToList();
+        }
+
+        private void Stage1() {
+            List<Tuple<Direction, int>> course = GetCourse("Day02Stage01.txt");
+            int x = 0, z = 0;
+            foreach (Tuple<Direction, int> step in course) {
+                switch (step.Item1) {
+                    case Direction.forward:
+                        x += step.Item2;
+                        break;
+                    case Direction.up:
+                        z -= step.Item2;
+                        break;
+                    case Direction.down:
+                        z += step.Item2;
+                        break;
+                }
+            }
+            int result = x * z;
+            Console.WriteLine($"Result: {result}");
+        }
+
+        private void Stage2() {
+            List<Tuple<Direction, int>> course = GetCourse("Day02Stage02.txt");
+            int x = 0, z = 0, aim = 0;
+            foreach (Tuple<Direction, int> step in course) {
+                switch (step.Item1) {
+                    case Direction.forward:
+                        x += step.Item2;
+                        z += aim * step.Item2;
+                        break;
+                    case Direction.up:
+                        aim -= step.Item2;
+                        break;
+                    case Direction.down:
+                        aim += step.Item2;
+                        break;
+                }
+            }
+            int result = x * z;
+            Console.WriteLine($"Result: {result}");
+        }
+
+        private enum Direction {
+            forward,
+            down,
+            up
+        }
+    }
+}
